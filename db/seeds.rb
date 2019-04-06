918da162069793730453decc9ec6b98af3003bed
tpr1992 <terence.pataneronan@gmail.com>
2019-04-06 15:30:32 -0400

    All tests passing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Student.create(first_name: "Terence", last_name: "Patane-Ronan")
+Student.create(first_name: "John", last_name: "Pena")
+Student.create(first_name: "Daniel", last_name: "Ko")
+Student.create(first_name: "Jennifer", last_name: "Gregorio")
+Student.create(first_name: "Ludwig", last_name: "Patane-Ronan")
